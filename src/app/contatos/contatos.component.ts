5883411a93ac228e0612e339ce874841be08f87c
Marcos Damasceno <luansousa9141@gmail.com>
2025-02-11 16:03:15 -0400

    adicao de redes sociais

--- src/app/contatos/contatos.component.ts
+++ src/app/contatos/contatos.component.ts
@@ -15,10 +15,16 @@ export class ContatosComponent {
 
   onSubmit(form: any) {
     if (form.valid) {
-      // Simule o envio do formulário (exemplo: pode ser uma chamada de API aqui)
+      const name = form.value.name;
+      const email = form.value.email;
+      const message = form.value.message;
+      const mailtoLink = `mailto:marcos.damascenorr@gmail.com?subject=Contato de ${name}&body=${message} (Email: ${email})`;
+
+      window.location.href = mailtoLink;
+
       this.isSuccess = true;
-      this.submitMessage = 'Mensagem enviada com sucesso!';
-      form.reset(); // Reseta o formulário
+      this.submitMessage = 'Redirecionando para o cliente de e-mail...';
+      form.reset(); 
     } else {
       this.isSuccess = false;
       this.submitMessage = 'Por favor, preencha todos os campos corretamente.';
