ce5c8b6abaeb93758cb25d7201a240217675d534
Marcos Damasceno <luansousa9141@gmail.com>
2025-02-13 20:28:05 -0400

    Adicao do darkmode

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,16 +1,27 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
   isMenuOpen = false;
+  isDarkMode = false;
 
   toggleMenu() {
     this.isMenuOpen = !this.isMenuOpen;
+  }
+
+  toggleDarkMode() {
+    this.isDarkMode = !this.isDarkMode;
+    if (this.isDarkMode) {
+      document.documentElement.classList.add('dark');
+    } else {
+      document.documentElement.classList.remove('dark');
+    }
   }
 }
